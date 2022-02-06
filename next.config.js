9ceeb7c7361b64905eb3ac9bee5deef8eeec078b
hectorbalan <balan.huchin.3@gmail.com>
2022-02-06 17:43:14 -0600

    round three

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
 const nextConfig = {
   target: "serverless",
-
+  assetPrefix: "https://hector4like6gorillaz.github.io/grass",
+  basePath: "https://hector4like6gorillaz.github.io/grass",
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
