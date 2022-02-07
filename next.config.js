95399c552fa2dc68174a690712e5114cd22cf4d4
hectorbalan <balan.huchin.3@gmail.com>
2022-02-06 18:14:06 -0600

    nine

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
 const nextConfig = {
   assetPrefix: "/grass/",
-  basePath: "/grass",
+  basePath: "https://github.com/hector4like6gorillaz",
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
