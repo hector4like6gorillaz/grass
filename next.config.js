c8ffd4320ae84f46667f6e0fa73ee8aa551f5953
hectorbalan <balan.huchin.3@gmail.com>
2022-02-06 20:33:39 -0600

    adasdasd

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/grass',
-  assetPrefix: '/grass/',
+  
+  assetPrefix: 'https://cdn.statically.io/gh/hector4like6gorillaz.github.io/grass/',
   images: {
     disableStaticImages: true,
   },
